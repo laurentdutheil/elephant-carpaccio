4db6a9f9a3eaea427d63f414820dc3b2edd099db
Laurent Dutheil <ldutheil74@gmail.com>
2023-10-09 19:42:41 +0200

    add id to UserStory

--- domain/team.go
+++ domain/team.go
@@ -6,6 +6,7 @@ type Team struct {
 }
 
 type UserStory struct {
+	id          string
 	description string
 	valuePoint  int
 	done        bool
@@ -17,24 +18,24 @@ func NewTeam(name string) *Team {
 
 func defaultBacklog() []UserStory {
 	return []UserStory{
-		{description: "Hello World", valuePoint: 1},
-		{description: "Can fill parameters", valuePoint: 1},
-		{description: "Compute order value without tax", valuePoint: 1},
-		{description: "Can handle float for 'number of items' AND 'price by item'", valuePoint: 1},
-		{description: "Tax for UT", valuePoint: 1},
-		{description: "Tax for NV", valuePoint: 1},
-		{description: "Tax for TX", valuePoint: 1},
-		{description: "Tax for AL", valuePoint: 1},
-		{description: "Tax for CA", valuePoint: 1},
-		{description: "15% Discount", valuePoint: 1},
-		{description: "10% Discount", valuePoint: 1},
-		{description: "7% Discount", valuePoint: 1},
-		{description: "5% Discount", valuePoint: 1},
-		{description: "3% Discount", valuePoint: 1},
-		{description: "Can handle rounding for result (two digits after the decimal point)", valuePoint: 1},
-		{description: "Prompts are clear. Display currency", valuePoint: 1},
-		{description: "Display details (order value, tax, discount", valuePoint: 1},
-		{description: "Do not have to re-launch the application for each test", valuePoint: 1},
+		{id: "EC-001", description: "Hello World", valuePoint: 1},
+		{id: "EC-002", description: "Can fill parameters", valuePoint: 1},
+		{id: "EC-003", description: "Compute order value without tax", valuePoint: 1},
+		{id: "EC-004", description: "Can handle float for 'number of items' AND 'price by item'", valuePoint: 1},
+		{id: "EC-005", description: "Tax for UT", valuePoint: 1},
+		{id: "EC-006", description: "Tax for NV", valuePoint: 1},
+		{id: "EC-007", description: "Tax for TX", valuePoint: 1},
+		{id: "EC-008", description: "Tax for AL", valuePoint: 1},
+		{id: "EC-009", description: "Tax for CA", valuePoint: 1},
+		{id: "EC-010", description: "15% Discount", valuePoint: 1},
+		{id: "EC-011", description: "10% Discount", valuePoint: 1},
+		{id: "EC-012", description: "7% Discount", valuePoint: 1},
+		{id: "EC-013", description: "5% Discount", valuePoint: 1},
+		{id: "EC-014", description: "3% Discount", valuePoint: 1},
+		{id: "EC-015", description: "Can handle rounding for result (two digits after the decimal point)", valuePoint: 1},
+		{id: "EC-016", description: "Prompts are clear. Display currency", valuePoint: 1},
+		{id: "EC-017", description: "Display details (order value, tax, discount", valuePoint: 1},
+		{id: "EC-018", description: "Do not have to re-launch the application for each test", valuePoint: 1},
 	}
 }
 
