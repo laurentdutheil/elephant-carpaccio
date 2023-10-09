e5d7b567e104971331245dc0ab798ee6cf86faac
Laurent Dutheil <ldutheil74@gmail.com>
2023-10-09 17:35:34 +0200

    team have a default backlog at beginning

--- domain/team.go
+++ domain/team.go
@@ -1,13 +1,47 @@
 package domain
 
 type Team struct {
-	name string
+	name    string
+	backlog []UserStory
+}
+
+type UserStory struct {
+	description string
+	valuePoint  int
+	done        bool
 }
 
 func NewTeam(name string) *Team {
-	return &Team{name: name}
+	return &Team{name: name, backlog: defaultBacklog()}
+}
+
+func defaultBacklog() []UserStory {
+	return []UserStory{
+		{description: "Hello World", valuePoint: 1},
+		{description: "Can fill parameters", valuePoint: 1},
+		{description: "Compute order value without tax", valuePoint: 1},
+		{description: "Can handle float for 'number of items' AND 'price by item'", valuePoint: 1},
+		{description: "Tax for UT", valuePoint: 1},
+		{description: "Tax for NV", valuePoint: 1},
+		{description: "Tax for TX", valuePoint: 1},
+		{description: "Tax for AL", valuePoint: 1},
+		{description: "Tax for CA", valuePoint: 1},
+		{description: "15% Discount", valuePoint: 1},
+		{description: "10% Discount", valuePoint: 1},
+		{description: "7% Discount", valuePoint: 1},
+		{description: "5% Discount", valuePoint: 1},
+		{description: "3% Discount", valuePoint: 1},
+		{description: "Can handle rounding for result (two digits after the decimal point)", valuePoint: 1},
+		{description: "Prompts are clear. Display currency", valuePoint: 1},
+		{description: "Display details (order value, tax, discount", valuePoint: 1},
+		{description: "Do not have to re-launch the application for each test", valuePoint: 1},
+	}
 }
 
 func (t Team) Name() string {
 	return t.name
+}
+
+func (t Team) Backlog() []UserStory {
+	return t.backlog
 }
--- domain/team_test.go
+++ domain/team_test.go
@@ -12,3 +12,38 @@ func TestTeamHaveAName(t *testing.T) {
 
 	assert.Equal(t, "A Team", teamName)
 }
+
+func TestTeamHaveADefaultBacklogAtBeginning(t *testing.T) {
+	team := NewTeam("A Team")
+
+	backlog := team.Backlog()
+
+	assert.Contains(t, backlog, UserStory{description: "Hello World", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Can fill parameters", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Compute order value without tax", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Can handle float for 'number of items' AND 'price by item'", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Tax for UT", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Tax for NV", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Tax for TX", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Tax for AL", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Tax for CA", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "15% Discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "10% Discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "7% Discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "5% Discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "3% Discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Can handle rounding for result (two digits after the decimal point)", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Prompts are clear. Display currency", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Display details (order value, tax, discount", valuePoint: 1})
+	assert.Contains(t, backlog, UserStory{description: "Do not have to re-launch the application for each test", valuePoint: 1})
+}
+
+func TestAllTheTeamBacklogAreNotDoneAtBeginning(t *testing.T) {
+	team := NewTeam("A Team")
+
+	backlog := team.Backlog()
+
+	for _, story := range backlog {
+		assert.False(t, story.done)
+	}
+}
