0d75ebe66bdaf31d9e3ca8285be746333139bf72
Laurent Dutheil <ldutheil74@gmail.com>
2023-10-10 18:51:14 +0200

    add a template renderer to display board in a html page
    First increment.
    Use of approval test.

---
+++ http-server/scoreRender_test.go
@@ -0,0 +1,45 @@
+package http_server
+
+import (
+	"bytes"
+	"github.com/approvals/go-approval-tests"
+	"testing"
+
+	. "elephant_carpaccio/domain"
+)
+
+func TestScoreRender(t *testing.T) {
+	game := simulateGame()
+
+	scoreRenderer, _ := NewScoreRenderer()
+
+	t.Run("it renders the scores of the teams", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := scoreRenderer.RenderBoard(&buf, game); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
+
+func simulateGame() *Game {
+	game := NewGame()
+	game.Register("A Team")
+	game.Register("The fantastic four")
+
+	for _, team := range game.Teams() {
+		team.Done("EC-001", "EC-002", "EC-003")
+	}
+	game.LogIteration()
+	for _, team := range game.Teams() {
+		team.Done("EC-004", "EC-005")
+	}
+	game.LogIteration()
+	for _, team := range game.Teams() {
+		team.Done("EC-006", "EC-007", "EC-008")
+	}
+	game.LogIteration()
+	return game
+}
---
+++ http-server/scoreRenderer.go
@@ -0,0 +1,31 @@
+package http_server
+
+import (
+	"embed"
+	"html/template"
+	"io"
+
+	. "elephant_carpaccio/domain"
+)
+
+var (
+	//go:embed "templates/*"
+	templates embed.FS
+)
+
+type ScoreRenderer struct {
+	template *template.Template
+}
+
+func NewScoreRenderer() (*ScoreRenderer, error) {
+	t, err := template.ParseFS(templates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &ScoreRenderer{template: t}, nil
+}
+
+func (r ScoreRenderer) RenderBoard(w io.Writer, game *Game) error {
+	return r.template.ExecuteTemplate(w, "index.gohtml", game)
+}
